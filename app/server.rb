70030975624581f3ffbe31c6d23f38f17f138c85
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-04-20 15:58:25 +0100

    filerename

--- app/server.rb
+++ app/server.rb
@@ -5,12 +5,6 @@ class RateMyPothole < Sinatra::Base
   get '/' do
     'Hello RateMyPothole!'
   end
-  get '/users/new' do
-    erb :"users/new"
-  end
-  get '/sessions/new' do
-    erb :"sessions/new"
-  end
   # start the server if ruby file executed directly
   run! if app_file == $PROGRAM_NAME
 
