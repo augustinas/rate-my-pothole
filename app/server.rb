6c1e6f50e5b4e9f7db7702ba3db4982a9585fbac
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-04-20 15:53:57 +0100

    Structure and Styling

--- app/server.rb
+++ app/server.rb
@@ -5,7 +5,12 @@ class RateMyPothole < Sinatra::Base
   get '/' do
     'Hello RateMyPothole!'
   end
-
+  get '/users/new' do
+    erb :"users/new"
+  end
+  get '/sessions/new' do
+    erb :"sessions/new"
+  end
   # start the server if ruby file executed directly
   run! if app_file == $PROGRAM_NAME
 
