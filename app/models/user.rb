2f22d6b7f5877217b167459ad68f376bcea53312
Joseph Knowles <joejknowles@gmail.com>
2015-04-21 17:31:38 +0100

    Javascript min password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,17 @@ require 'bcrypt'
 
 class User
 
+  MIN_PSWD_LENGTH = 6
   include DataMapper::Resource
 
   property :id, Serial
-  property :username, String, unique: true, message: 'Sorry, This username already exists!'
-  property :email, String, unique: true, message: 'Sorry, A user with this email already exists!'
+  property :username, String, unique: true,
+                              message: 'Sorry, This username already exists!'
+  property :email, String, unique: true,
+                           message: 'Sorry, A user with'\
+                           ' this email already exists!'
   property :password_digest, Text, required: true,
-                                   message: 'Sorry, there was something'\
-                                     ' wrong with your password!'
+                                   message: 'Sorry, your password was too short'
 
   attr_reader :password
   attr_accessor :password_confirmation
@@ -18,7 +21,7 @@ class User
                             message: 'Sorry your passwords did not match!'
 
   def password=(password)
-    return nil if password.empty?
+    return nil if password.length < MIN_PSWD_LENGTH
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
