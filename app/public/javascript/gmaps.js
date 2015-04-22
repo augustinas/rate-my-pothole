41b6588429b0e6ba8a0e2c595b2525d785ef7f64
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-04-22 18:18:47 +0100

    Setup Jasmine-gem testing, written unit test for AJAX call

--- app/public/javascript/gmaps.js
+++ app/public/javascript/gmaps.js
@@ -1,24 +1,49 @@
-var geocoder, map;
+var Gmaps = function() {
+  this.geocoder;
+  this.map;
+}
 
-function codeAddress(address) {
-  geocoder = new google.maps.Geocoder();
-  geocoder.geocode({
-    'address': address
-  }, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-      var myOptions = {
-        zoom: 8,
-        center: results[0].geometry.location,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-      map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+Gmaps.prototype.initialize = function() {
+  var mapOptions = {
+  zoom: 8,
+  center: {lat: -33.8666, lng: 151.1958}
+  }
+  this.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  // this.centerMap("Reading Rd, London, UK");
+  // this.markMap("LS9 8PA");
+};
 
-      var marker = new google.maps.Marker({
-        map: map,
-        position: reults[0].geometry.map
-      });
-
-      google.maps.event.addDomListener(window, 'load', initialize);
+Gmaps.prototype.centerMap = function(address) {
+  this.geocoder = new google.maps.Geocoder();
+  this.geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      this.map.setCenter(results[0].geometry.location);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
     }
   });
-}
+};
+
+Gmaps.prototype.markMap = function(address) {
+  this.geocoder = new google.maps.Geocoder();
+  this.geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var marker = new google.maps.Marker({
+        map: this.map,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+};
+
+Gmaps.prototype.getLatLong = function(address) {
+  $.ajax({
+    url: 'http://maps.googleapis.com/maps/api/geocode/json'
+  });
+};
+
+var gmaps = new Gmaps();
+
+google.maps.event.addDomListener(window, 'load', gmaps.initialize);
