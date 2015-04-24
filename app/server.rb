1ff35c882c28b90c741aac467dafcf004cea406f
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-04-24 10:52:18 +0100

    commit for gh

--- app/server.rb
+++ app/server.rb
@@ -42,11 +42,13 @@ class RateMyPothole < Sinatra::Base
   post '/users' do
     user = new_user(params)
     if user.save
+      p "USER SAVED"
       session[:user_id] = user.id
       flash[:notice] = "Welcome, #{user.username}!"
       redirect to '/'
     else
       flash[:errors] = user.errors.full_messages
+      p user.errors.full_messages
       redirect '/users/new'
     end
   end
